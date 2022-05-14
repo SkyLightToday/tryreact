d82f8024bb7739b8f60a68cf4ffe0a7fc2c6e208
SkyLightToday <yulya.kolechkina83648@yandex.ru>
2022-05-14 14:18:14 +0300

    first today

--- src/Redux/profile-reducer.js
+++ src/Redux/profile-reducer.js
@@ -2,17 +2,34 @@ const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 const profileReducer = (state, action) => {
-    if(action.type === ADD_POST) {
-        let newPost = {
-          id: 5, message: state.newPostText, likesCount: 0
-        };
-        state.posts.push(newPost);
-        state.newPostText = '';//зануление.очистка поля ввода
-      } else if(action.type === UPDATE_NEW_POST_TEXT) {
-          state.newPostText = action.newText;
-      }
+    switch(action.type) {
+        case ADD_POST:
+            let newPost = {
+                id: 5, message: state.newPostText, likesCount: 0
+            };
+            state.posts.push(newPost);
+            state.newPostText = '';//зануление.очистка поля ввода
+            return state;
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newText;
+            return state;
+            default:
+                return state;
+    }
+    // if(action.type === ADD_POST) {
+    //     let newPost = {
+    //       id: 5, message: state.newPostText, likesCount: 0
+    //     };
+    //     state.posts.push(newPost);
+    //     state.newPostText = '';//зануление.очистка поля ввода
+    //   } else if(action.type === UPDATE_NEW_POST_TEXT) {
+    //       state.newPostText = action.newText;
+    //   }
 
-    return state;
+    // return state;
 };
+
+export const addPostActionCreator = () => ({type: ADD_POST})
+export const updatePostActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text})
 
 export default profileReducer;
